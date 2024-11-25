e4341aab7ee63a397c5100492e1a8ee6b361ca9a
Helio Dourado <heliohdd@yahoo.com.br>
2024-11-25 08:26:31 -0300

    Estrutura do projeto com pages e navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
 import "./App.css";
 
-function App() {
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
-  return <><div className="App"><h1>Context API App</h1></div></>;
+import Navbar from "../../../8_REACT_ROUTER/react-router-app/src/components/Navbar";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Products from "./pages/Products";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <h1>Context API App</h1>
+        <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+        </BrowserRouter>
+      </div>
+    </>
+  );
 }
 
 export default App;
